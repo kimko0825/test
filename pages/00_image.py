1c15f973c92a3e857407518f7aba3521ea949637
kimko0825 <fukuokatabiok@gmail.com>
2025-06-21 13:14:58 +0900

    Update 00_image.py

--- pages/00_image.py
+++ pages/00_image.py
@@ -1,2 +1,2 @@
-import streamlot as st
+import streamlit as st
 st.images("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWcybjV1d3J3cWd0Ymdudzd1cHVrdjhzODZienZqamNkZzRoeW5jYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7NON3hH96H4InrrkPy/giphy.gif")
